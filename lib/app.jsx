04a0ed9ec39fd705f9bf20b88db1d374d4a1f308
LI Jing <jing.li@devinci.fr>
2015-11-17 15:35:51 +0100

    step 5

--- lib/app.jsx
+++ lib/app.jsx
@@ -13,18 +13,25 @@ var Box = React.createClass({
      * Render a HTML button
      * @return {ReactElement}
      */
-        //'getInitialState':function onGetInitialState(){
-        //return {
-        //    "value": this.props.initialValue
-        //}
-    //},
+        'getInitialState':function onGetInitialState(){
+        return {
+            "value": this.props.initialValue
+        }
+    },
+        componentWillMount: function() {
+            var serf = this;
+            this.timer = setInterval(function(){
+                serf.setState({'value' : serf.state.value =='X'? '0':'X'});
+            },300)
+        },
+    
         'render': function onRender () {
             return (
-                <button style={buttonStyle}>{this.props.value}</button>
+                <button style={buttonStyle}>{this.state.value}</button>
             )
         }
 
 });
 
 // here we ask React to add the component Box into the body
-React.render(<Box value='X'/>, document.body);
+React.render(<Box value='Y'/>, document.body);
