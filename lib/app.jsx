df29f3ecfbb9b427ebdc462992079cfcc92ba114
LI Jing <jing.li@devinci.fr>
2015-11-17 14:21:27 +0100

    step 4

--- lib/app.jsx
+++ lib/app.jsx
@@ -2,16 +2,31 @@
 
 var React = require('react');
 
+//CSS styl efor the button
+var buttonStyle = {
+    'height': '100px',
+    'width': '100px'
+};
+
 var Box = React.createClass({
-  /**
-   * Render a HTML button
-   * @return {ReactElement}
-   */
-  'render': function onRender () {
-    return (
-      <button>X</button>
-    );
-  }
+    /**
+     * Render a HTML button
+     * @return {ReactElement}
+     */
+        'getInitialState':function onGetInitialState(){
+        return {
+            "value": this.props.initialValue
+        }
+
+
+        },
+        'render': function onRender () {
+            return (
+                <button style={buttonStyle}>{this.props.value}</button>
+            )
+        }
+
 });
 
-React.render(<Box/>, document.body);
+// here we ask React to add the component Box into the body
+React.render(<Box value='X'/>, document.body);
