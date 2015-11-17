7c0758aea84e7a3d63878d369e92239a7ee4ac81
LI Jing <jing.li@devinci.fr>
2015-11-17 15:52:13 +0100

    step 6

--- lib/app.jsx
+++ lib/app.jsx
@@ -2,7 +2,7 @@
 
 var React = require('react');
 
-//CSS styl efor the button
+//CSS style for the button
 var buttonStyle = {
     'height': '100px',
     'width': '100px'
@@ -18,20 +18,25 @@ var Box = React.createClass({
             "value": this.props.initialValue
         }
     },
-        componentWillMount: function() {
+        handleOnclick: function() {
             var serf = this;
-            this.timer = setInterval(function(){
-                serf.setState({'value' : serf.state.value =='X'? '0':'X'});
-            },300)
+            serf.setState({'value' : serf.state.value =='X'? '0':'X'});
         },
-    
+
+    componentDidMount: function() {
+        window.addEventListener('click', this.handleOnclick);
+    },
+
+    componentWillUnmount: function() {
+        window.removeEventListener('click', this.handleOnclick);
+    },
         'render': function onRender () {
             return (
-                <button style={buttonStyle}>{this.state.value}</button>
+                <button style={buttonStyle} onclick={this.handleOnclick}>{this.state.value}</button>
             )
         }
 
 });
 
 // here we ask React to add the component Box into the body
-React.render(<Box value='Y'/>, document.body);
+React.render(<Box value='X'/>, document.body);
